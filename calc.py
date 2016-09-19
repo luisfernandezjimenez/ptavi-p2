28971af23ccd18dddab2a0fa0430f12c9fdc0efa
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-09-19 11:06:03 +0200

    añadimos las operaciones multiplicacion y division a nuestra calculadora

--- calc.py
+++ calc.py
@@ -13,6 +13,15 @@ def minus(op1, op2):
     """ Function to substract the operands """
     return op1 - op2
 
+
+def mult(op1, op2):
+    return op1 * op2
+
+
+def div(op1, op2):
+    return op1 / op2
+
+
 if __name__ == "__main__":
     try:
         operando1 = int(sys.argv[1])
@@ -24,7 +33,11 @@ if __name__ == "__main__":
         result = plus(operando1, operando2)
     elif sys.argv[2] == "resta":
         result = minus(operando1, operando2)
+    elif sys.argv[2] == "multiplicacion":
+        result = mult(operando1, operando2)
+    elif sys.argv[2] == "division":
+        result = div(operando1, operando2)
     else:
-        sys.exit('Operación sólo puede ser sumar o restar.')
+        sys.exit('Operación sólo puede ser suma, resta, multiplicacion o division.')
 
     print(result)
