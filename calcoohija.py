9afcedfc0f59f9a53144191083959855c4ee5501
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-09-21 12:58:37 +0200

    calculadora orientada a objetos con las operaciones de multiplicaciones y divisiones

--- calcoohija.py
+++ calcoohija.py
@@ -5,22 +5,23 @@ import sys
 
 import calcoo
 
-class CalculadoraHija(calcoo.Calculadora): 
-    def multiplicar (self, op1, op2):
+
+class CalculadoraHija(calcoo.Calculadora):
+    def multiplicar(self, op1, op2):
         return op1 * op2
-    
-    def dividir (self, op1, op2):
-    	if op2 == 0:
-    		sys.exit("Division by zero is not allowed")
-    	else:
-    		return op1 / op2
+
+    def dividir(self, op1, op2):
+        if op2 == 0:
+            sys.exit("Division by zero is not allowed")
+        else:
+            return op1 / op2
 
 if __name__ == "__main__":
     calculadora = CalculadoraHija()
-    
+
     try:
-       operando1 = int(sys.argv[1])
-       operando2 = int(sys.argv[3])
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
@@ -29,10 +30,10 @@ if __name__ == "__main__":
 
     elif sys.argv[2] == "resta":
         resultado = calculadora.resta(operando1, operando2)
-    
+
     elif sys.argv[2] == "multiplica":
         resultado = calculadora.multiplicar(operando1, operando2)
-    
+
     elif sys.argv[2] == "divide":
         resultado = calculadora.dividir(operando1, operando2)
 
@@ -40,4 +41,3 @@ if __name__ == "__main__":
         sys.exit('Operación sólo puede ser suma, resta, mutiplica y divide.')
 
     print(resultado)
-
