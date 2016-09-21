8939b3b8ba37c577dcc777c588d9ecfbc935ef40
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-09-21 13:02:20 +0200

    calculadora que lee de ficheros para realizar las operaciones

--- calcplus.py
+++ calcplus.py
@@ -4,24 +4,24 @@
 import sys
 import calcoohija
 
-    
+
 if __name__ == "__main__":
     calculadora = calcoohija.CalculadoraHija()
 
-    fich = open(sys.argv[1],'r')
+    fich = open(sys.argv[1], 'r')
 
     lineas = fich.readlines()
 
     for linea in lineas:
         elementos = linea.split(',')
-    
+
         resultado = int(elementos[1])
         operacion = elementos[0]
         elementos = elementos[2:]
-    
+
         for operandos in elementos:
-        
-            if operacion == "suma":           
+
+            if operacion == "suma":
                 resultado = calculadora.suma(resultado, int(operandos))
             elif operacion == "resta":
                 resultado = calculadora.resta(resultado, int(operandos))
@@ -30,18 +30,7 @@ if __name__ == "__main__":
             elif operacion == "divide":
                 resultado = calculadora.dividir(resultado, int(operandos))
             else:
-                sys.exit('Operación sólo puede ser suma, resta, mutiplica y divide.')
-        
+                sys.exit('Operación sólo puede ser suma, resta, mutiplica'
+                         'y divide.')
+
         print(resultado)
-
-
-
-
-
-
-
-
-
-    
-    
-
