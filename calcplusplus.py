40c59a019e26ae344af35906e89ff5572994b564
Luis  Fernandez Jimenez <lfernand@gamma16.aulas.gsyc.urjc.es>
2016-09-26 09:52:19 +0200

    misma funcionalidad que calcplus pero con el modulo csv y sentencia with

---
+++ calcplusplus.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import calcoohija
+import csv 
+
+
+        
+if __name__ == "__main__":
+    calculadora = calcoohija.CalculadoraHija()
+
+    with open(sys.argv[1], newline='\n') as f:
+        lineas = csv.reader(f)
+        for linea in lineas:
+            resultado = int(linea[1])
+            operacion = linea[0]
+            elementos = linea[2:]
+
+            for operandos in elementos:
+
+                if operacion == "suma":
+                    resultado = calculadora.suma(resultado, int(operandos))
+                elif operacion == "resta":
+                    resultado = calculadora.resta(resultado, int(operandos))
+                elif operacion == "multiplica":
+                    resultado = calculadora.multiplicar(resultado, int(operandos))
+                elif operacion == "divide":
+                    resultado = calculadora.dividir(resultado, int(operandos))
+                else:
+                    sys.exit('Operación sólo puede ser suma, resta, mutiplica'
+                             'y divide.')
+
+            print(resultado)
